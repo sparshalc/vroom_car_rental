3bb471d7e2d76a0ba6d3517bf1e0cd9cf25fb36b
krizam <krizambyanju@gmail.com>
2024-03-17 23:06:17 +0545

    Add phone_number attribute && validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,16 @@ class User < ApplicationRecord
   enum role: { user: 0, seller: 1, admin: 2 }
 
   validate :avatar_content_type, if: :avatar_attached?
+  validates :full_name, presence: true, uniqueness: { message: "has already been taken try again with a new one." }
+  validate :phone_number_format
+
+  private
+
+  def phone_number_format
+    unless phone_number =~ /^\d{10}$/
+      errors.add(:phone_number, "must be 10 digits")
+    end
+  end
 
   def avatar_content_type
     unless valid_content_type.include?(avatar.blob.content_type)
@@ -21,4 +31,5 @@ class User < ApplicationRecord
   def avatar_attached?
     avatar.attached?
   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_17_100809) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_17_165048) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_17_100809) do
     t.datetime "updated_at", null: false
     t.string "full_name"
     t.integer "role", default: 0
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
