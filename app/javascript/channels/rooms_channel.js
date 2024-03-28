8c911a17d5f95f5bf2f13bcc8032ecd3c908a30f
sparshalc <imailsparsha@gmail.com>
2024-03-28 21:19:26 +0545

    resolve unauthorized connection attempt rejection in ActionCable

--- app/javascript/channels/rooms_channel.js
+++ app/javascript/channels/rooms_channel.js
@@ -1,18 +1,20 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create("RoomsChannel", {
-  connected() {
-  },
+const metaTag = document.querySelector("meta[name='current-user']");
 
-  disconnected() {
-  },
-
-  received(data) {
-    const room_id = data.room_id
-    const new_name = data.room_name
-    const title = document.getElementById(`name_room_${room_id}`)
-    if(title){
-      title.innerHTML = new_name
-    }
-  }
-});
+if (metaTag !== null) {
+    consumer.subscriptions.create("RoomsChannel", {
+        connected() {
+        },
+        disconnected() {
+        },
+        received(data) {
+            const room_id = data.room_id;
+            const new_name = data.room_name;
+            const title = document.getElementById(`name_room_${room_id}`);
+            if (title) {
+                title.innerHTML = new_name;
+            }
+        }
+    });
+}
