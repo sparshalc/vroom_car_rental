79e812b3411e536e594996c138e6249650d89933
Sushh55 <shushant.p12@gmail.com>
2024-04-04 17:49:01 +0545

    Fix action cable and refactor css

--- app/javascript/channels/notification_count_channel.js
+++ app/javascript/channels/notification_count_channel.js
@@ -1,26 +1,30 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create("NotificationCountChannel", {
-  connected() {
-  },
+const metaTag = document.querySelector("meta[name='current-user']");
 
-  disconnected() {
-  },
+if (metaTag !== null) {
+  consumer.subscriptions.create("NotificationCountChannel", {
+    connected() {
+    },
 
-  received(data) {
-    const user_id = document.querySelectorAll('#user-id')
-    const bottom_flash = document.getElementById('btn-flash')
-    const bottom_flash_message = document.getElementById('btn-flash-msg')
-    const notification_count = document.getElementById(`notification_count_${data.car_user}`)
-    
-    if(user_id[0].innerHTML == data.car_user){
-      notification_count.innerHTML = data.notifications
-      bottom_flash_message.innerHTML = `<i class="bi bi-chat-square-text-fill"></i> <strong>${data.user.email}</strong> commented on your post (${data.car_name})`
-      bottom_flash.classList.add('active')
+    disconnected() {
+    },
+
+    received(data) {
+      const user_id = document.querySelectorAll('#user-id')
+      const bottom_flash = document.getElementById('btn-flash')
+      const bottom_flash_message = document.getElementById('btn-flash-msg')
+      const notification_count = document.getElementById(`notification_count_${data.car_user}`)
       
-      setTimeout(()=>{
-        bottom_flash.classList.remove('active')
-      }, 3000)
+      if(user_id[0].innerHTML == data.car_user){
+        notification_count.innerHTML = data.notifications
+        bottom_flash_message.innerHTML = `<i class="bi bi-chat-square-text-fill"></i> <strong>${data.user.email}</strong> commented on your post (${data.car_name})`
+        bottom_flash.classList.add('active')
+        
+        setTimeout(()=>{
+          bottom_flash.classList.remove('active')
+        }, 3000)
+      }
     }
-  }
-});
+  })
+}
