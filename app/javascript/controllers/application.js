8eb725a79e6c9ffab3c90b383e50d434b96221f7
sparshalc <imailsparsha@gmail.com>
2024-04-13 10:43:27 +0545

    Fix swiper bug with turbo

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -2,18 +2,26 @@ import { Application } from "@hotwired/stimulus"
 
 const application = Application.start()
 
-// Configure Stimulus development experience
 application.debug = false
+
 window.Stimulus   = application
 
 export { application }
 
-const swiper = new Swiper('.swiper', {
-  loop: true,
-
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
+document.addEventListener('turbo:load', () => {
+  const swiperElements = document.querySelectorAll('.swiper');
+  
+  swiperElements.forEach(swiperElement => {
+    new Swiper(swiperElement, {
+      pagination: {
+        el: swiperElement.querySelector('.swiper-pagination'),
+      },
+      loop: true,
+      
+      navigation: {
+        nextEl: swiperElement.querySelector('.swiper-button-next'),
+        prevEl: swiperElement.querySelector('.swiper-button-prev'),
+      },
+    });
+  });
 });
