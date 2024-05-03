d1217f6582d9cf0ef5a412a216f2de8d9f94869d
sparshalc <imailsparsha@gmail.com>
2024-05-04 01:43:25 +0545

    Add Schema & Implement Turbo for newsletter

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,4 +3,11 @@ class UserController < ApplicationController
     @bookings = current_user.bookings.all
   end
 
+  def subscribe_to_newsletter
+    current_user.update(subscribed_to_newsletter: true)
+
+    respond_to do |format|
+      format.turbo_stream { flash.now[:notice] = "Congratulations! #{current_user.full_name}. You are now subscribed to Vroom's Newsletter 📰" }
+    end
+  end
 end
